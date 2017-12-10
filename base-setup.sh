4e91a357a925322059a1dd561739eabdd6543c99
GPanula <github@kablah.com>
2017-12-10 10:49:26 -0600

    avoid prompts when deleting files

--- base-setup.sh
+++ base-setup.sh
@@ -7,9 +7,9 @@ cd /etc
 unzip /tmp/build/skel.zip
 mv /etc/skel /etc/skel.orig
 mv /etc/skel-master /etc/skel
-rm /etc/skel/LICENSE 
-rm /etc/skel/.LICENSE
-rm /etc/skel/README.md
+rm -f /etc/skel/LICENSE 
+rm -f /etc/skel/.LICENSE
+rm -f /etc/skel/README.md
 chmod 640 /etc/skel/.ssh/authorized_keys2
 
 cd /root
@@ -20,7 +20,7 @@ wget -O /tmp/build/profile.d.zip https://github.com/gpanula/profile.d/archive/ma
 cd /tmp/build
 unzip /tmp/build/profile.d.zip
 cp /tmp/build/profile.d-master/* /etc/profile.d/
-rm /etc/profile.d/README.md
+rm -f /etc/profile.d/README.md
 
 cd /etc/ssh
 mv sshd_config sshd_config.orig
