40d458dc07ca8d03e7143d2df09d097943f86a87
GPanula <github@kablah.com>
2017-12-10 10:54:40 -0600

    skip the git config bit for root .bashrc

--- base-setup.sh
+++ base-setup.sh
@@ -15,6 +15,7 @@ chmod 640 /etc/skel/.ssh/authorized_keys2
 cd /root
 mv .bashrc .bashrc.orig
 cp /etc/skel/.bashrc /root/.bashrc
+sed -i '/set-git-info/s/^/#/' /root/.bashrc
 
 wget -O /tmp/build/profile.d.zip https://github.com/gpanula/profile.d/archive/master.zip
 cd /tmp/build
