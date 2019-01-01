a214b2d3d2942a702cf8bb76fd75b2ce77f62bc0
GPanula <github@kablah.com>
2019-01-01 13:45:28 -0600

    add script for sync'ing profile.d

---
+++ sync-profile_d.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# need git for this to work
+[ ! $( which git ) ] && exit 99
+
+# grab profile.d repo
+git clone https://github.com/gpanula/profile.d.git  /tmp/profile.d.$$
+cd /tmp/profile.d.$$
+git checkout master
+# sub module ref: https://blog.github.com/2016-02-01-working-with-submodules/
+git submodule update --init --recursive
+
+cd /tmp
+rsync -ah --exclude ".git*" /tmp/profile.d.$$/ /etc/profile.d/
+
+echo profile.d syncd with github repo | logger
+
+rm -rf /tmp/profile.d.$$
+
+exit
