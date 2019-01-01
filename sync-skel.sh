4ac4aee4114620812c837f6666e7f44d06a51f2c
GPanula <github@kablah.com>
2019-01-01 14:33:53 -0600

    add bits for updating root dot files

--- sync-skel.sh
+++ sync-skel.sh
@@ -9,12 +9,22 @@ cd /tmp/skel.$$
 git checkout master
 rm -f LICENSE
 rm -f README.md
-cp -f .bashrc /root/.bashrc
 
 cd /tmp
 rsync -ah --exclude ".git*" /tmp/skel.$$/ /etc/skel/
 
 echo skel syncd with github repo | logger
+
+# update root's bits
+cp -f /etc/skel/.bashrc /root/.bashrc
+cp -f /etc/skel/.vimrc /root/.vimrc
+sed -i '/set-git-info/s/^/#/' /root/.bashrc
+mkdir /root/.ssh
+chmod 0600 /root/.ssh
+cp /etc/skel/.ssh/authorized_keys2 /root/.ssh/authorized_keys2
+ln -s /root/.ssh/authorized_keys2 /root/.ssh/authorized_keys
+
+echo root dot files updated | logger
 
 rm -rf /tmp/skel.$$
 
